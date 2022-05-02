1c342cfb93ade81612883f8c76005357f9cea35b
Hannah <94648556+hannahj25@users.noreply.github.com>
2022-05-02 16:05:08 +0930

    wrote route to get user by id

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -7,6 +7,17 @@ router.get('/users', (req, res) => {
     User.find()
     .then((users) => res.json(users))
     .catch((err) => res.status(500).json(err));
-})
+});
+
+router.get('/users/:userId', (req, res) => {
+    User.findOne({_id: req.params.userId})
+    .select('-__v')
+    .then((user) =>
+    !user
+    ? res.status(404).json({ message: 'No user with that ID!'})
+    : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
+});
 
 module.exports = router
