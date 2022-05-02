fe606842c779f1d052b5b3b24df02e00656d3ddb
Hannah <94648556+hannahj25@users.noreply.github.com>
2022-05-02 21:10:26 +0930

    wrote get routes for thoughts

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -3,3 +3,20 @@ const User = require("../../models/User");
 
 const router = require('express').Router();
 
+// Get all thoughts
+router.get('/thoughts', (req, res) => {
+    Thought.find()
+    .then((thoughts) => res.json(thoughts))
+    .catch((err) => res.status(500).json(err));
+});
+
+router.get('/thoughts/:thoughtId', (req, res) => {
+    Thought.findOne({_id: req.params.thoughtId})
+    .select('-__v')
+    .then((thought) =>
+    !thought
+    ? res.status(404).json({ message: 'No thought with that Id!'})
+    : res.json(thought)
+    )
+    .catch((err) => res.status(500).json(err));
+});
